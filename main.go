423fbdc1514e22e0394772b4e84a29dc2b4558b6
konotorii <github@konotorii.com>
2024-09-24 00:45:29 -0400

    feat: Created main runtime file for CLI with flags and translation logic

--- main.go
+++ main.go
@@ -1,1 +1,111 @@
-package automated_word_translator
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	var translateLink string
+	var translateApiKey string
+	var file string
+	var fromLanguage string
+	var toLanguage string
+	var saveLocation string
+
+	cli.VersionPrinter = func(cCtx *cli.Context) {
+		fmt.Printf("version=%s revision=%s\n", "0.0.0", "REV") // TODO
+	}
+
+	app := &cli.App{
+		Name:  "Automated Word Translator",
+		Usage: "Automatically translate words in the contents of a file",
+		Authors: []*cli.Author{
+			&cli.Author{
+				Name:  "Konotorii",
+				Email: "github@konotorii.com",
+			},
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "translateLink",
+				Usage:       "Link to translation server",
+				Destination: &translateLink,
+				Required:    true,
+			},
+			&cli.StringFlag{
+				Name:        "translateApiKey",
+				Usage:       "API Key to translation server",
+				Destination: &translateApiKey,
+				Required:    true,
+			},
+			&cli.StringFlag{
+				Name:        "file", // Not actual file, just file contents
+				Usage:       "Contents of the file desired to read",
+				Destination: &file,
+				Required:    true,
+			},
+			&cli.StringFlag{
+				Name:        "fromLanguage",
+				Value:       "de",
+				Usage:       "Language to translate from",
+				Destination: &fromLanguage,
+				Required:    true,
+			},
+			&cli.StringFlag{
+				Name:        "toLanguage",
+				Value:       "en",
+				Usage:       "Language to translate to",
+				Destination: &toLanguage,
+				Required:    true,
+			},
+			&cli.StringFlag{
+				Name:        "saveLocation",
+				Usage:       "File location to save the final output at",
+				Destination: &saveLocation,
+				Required:    true,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			replaced := file
+
+			fileStrings := strings.Split(file, " ")
+			fileStringsLength := len(fileStrings)
+			var translatedStrings []map[string]string
+
+			println(fmt.Sprintf("Found %s words to translate...", FormatNumber(fileStringsLength)))
+
+			startTime := time.Now()
+
+			for i := 0; i < len(fileStrings); i++ {
+				translated := Translate(c, fileStrings[i], translateApiKey, fromLanguage, toLanguage)
+
+				translatedStrings = append(translatedStrings, map[string]string{"original": fileStrings[i], "new": translated})
+
+				replaced = strings.ReplaceAll(replaced, fileStrings[i], translated)
+
+				println(fmt.Sprintf("%s %s of %s words translated.", Percentage(float64(i), float64(fileStringsLength)), FormatNumber(i), FormatNumber(fileStringsLength)))
+			}
+
+			println(fmt.Sprintf("Translated %d words in %s.", len(translatedStrings), time.Since(startTime).String()))
+
+			err := os.WriteFile(saveLocation, []byte(replaced), 0644)
+			if err != nil {
+				log.Fatal(err)
+				return err
+			}
+
+			println(fmt.Sprintf("Saved translation to: %s", saveLocation))
+
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
