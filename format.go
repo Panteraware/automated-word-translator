a283f095c0f0dfc5f702772f0dfccc1374738369
konotorii <github@konotorii.com>
2024-10-08 23:01:22 -0400

    feat: Added a few extra replace all formats

--- format.go
+++ format.go
@@ -1,12 +1,20 @@
 package main
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 func Format(text string) string {
 	// Removes all special characters from string
 	re, _ := regexp.Compile("/[^\\w\\s]/gi")
 
 	text = re.FindString(text)
+
+	text = strings.ReplaceAll(text, "\n", "")
+	text = strings.ReplaceAll(text, "\r", " ")
+	text = strings.ReplaceAll(text, "#", "")
+	text = strings.ReplaceAll(text, "`", "")
 
 	// Still more replacements left
 
